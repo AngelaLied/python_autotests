3ff5c94f3cdf030f3b0f129ab230cc51d20fa12d
Angela Lieders <116493422+AngelaLied@users.noreply.github.com>
2022-12-05 16:53:14 +0300

    Update test_status.py

--- test_status.py
+++ test_status.py
@@ -3,10 +3,12 @@ import pytest
 def test_status_code():
     response = requests.get('https://pokemonbattle.me:5000/trainers')
     assert response.status_code == 200
+    print(response.text)
 
 def test_piece_of_body():
-    response = requests.get('https://pokemonbattle.me:5000/trainers', params = {'trainer_id' : '1190'})
+    response_get = requests.get('https://pokemonbattle.me:5000/trainers', params = {'trainer_id' : '1190'})
     assert response.json()['trainer_name'] == 'VilliVonka'
+    print(resonsse_get.text)
 
 
   
